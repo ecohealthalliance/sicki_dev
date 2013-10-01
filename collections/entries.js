3b642ceec0a63d7687e9008e6fb1c3590733a44b
nicopresto <nicopresto@gmail.com>
2013-09-30 20:26:08 -0400

    added dummy entries and set collection rules

--- collections/entries.js
+++ collections/entries.js
@@ -8,32 +8,32 @@ Entries.allow({
 Entries.deny({
   update: function(userId, entry, fieldNames) {
     // may only edit the following three fields:
-    return (_.without(fieldNames, 'lastName', 'firstName', 'location').length > 0);
+    return (_.without(fieldNames, 'eventName', 'disease', 'zoonoticType').length > 0);
   }
 });
 
 Meteor.methods({
   entry: function(entryAttributes) {
     var user = Meteor.user(),
-      entryWithSameLastName = Entries.findOne({lastName: entryAttributes.lastName});
+      entryWithSameEventName = Entries.findOne({eventName: entryAttributes.eventName});
 
     // ensure the user is logged in
     if (!user)
       throw new Meteor.Error(401, "You need to login to add new entries");
 
     // ensure the entry has a title
-    if (!entryAttributes.lastName)
-      throw new Meteor.Error(422, 'Please fill in a last name');
+    if (!entryAttributes.eventName)
+      throw new Meteor.Error(422, 'Please fill in an event name');
 
     // check that there are no previous entries with the same last name
-    if (entryAttributes.lastName && entryWithSameLastName) {
+    if (entryAttributes.eventName && entryWithSameEventName) {
       throw new Meteor.Error(302, 
-        'This last name has already been entered', 
-        entryWithSameLastName._id);
+        'This event name has already been entered', 
+        entryWithSameEventName._id);
     }
 
     // pick out the whitelisted keys
-    var entry = _.extend(_.pick(entryAttributes, 'lastName', 'firstName', 'location', 'description'), {
+    var entry = _.extend(_.pick(entryAttributes, 'eventName', 'disease', 'zoonoticType'), {
       userId: user._id, 
       author: user.username, 
       submitted: new Date().getTime(),
--- server/fixtures.js
+++ server/fixtures.js
@@ -8,57 +8,93 @@ if (Entries.find().count() === 0) {
   });
   var nico = Meteor.users.findOne(nicoId);
 
-  var amyId = Meteor.users.insert({
-    profile: { name: 'aslagle' }
+  var carlaId = Meteor.users.insert({
+    profile: { name: 'ctilchin' }
   });
-  var amy = Meteor.users.findOne(amyId);
+  var carla = Meteor.users.findOne(carlaId);
+
+  var alexaId = Meteor.users.insert({
+    profile: { name: 'afrank' }
+  });
+  var alexa = Meteor.users.findOne(alexaId);
+
+  var alexId = Meteor.users.insert({
+    profile: { name: 'ademarsh' }
+  });
+  var alex = Meteor.users.findOne(alexId);
+
+  var elizabethId = Meteor.users.insert({
+    profile: { name: 'eeckel' }
+  });
+  var elizabeth = Meteor.users.findOne(elizabethId);
 
   var surveyOneId = Entries.insert({
-    lastName: 'Martinez',
-    firstName: 'Juan',
-    location: 'San Lucas',
-    description: 'Was surveyed with Gonzalez',
-    userId: amy._id,
-    author: amy.profile.name,
+    eventName: 'Acinetobacter baumannii gentamycin-res (Australia, 1993)',
+    refEventName: 'Jones et al. SI',
+    disease: 'none reported, but likely nosocomial',
+    refDisease: '75',
+    zoonoticType: 'non-zoonotic',
+    refZoonoticType: '459',
+    userId: carla._id,
+    author: carla.profile.name,
     submitted: now - 7 * 3600 * 1000,
     commentsCount: 2
   });
 
+
   Comments.insert({
     entryId: surveyOneId,
     userId: nico._id,
     author: nico.profile.name,
-    submitted: now - 5 * 3600 * 1000,
-    body: 'Amy: may I review?'
+    submitted: now - 3 * 3600 * 1000,
+    body: 'Carla: are we sure this is nosocomial'
   });
 
   Comments.insert({
     entryId: surveyOneId,
-    userId: amy._id,
-    author: amy.profile.name,
-    submitted: now - 3 * 3600 * 1000,
-    body: 'Nico: you sure can!'
+    userId: carla._id,
+    author: carla.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'Nico: Australia 1993 reports nosocomial infection, not specified but likely septicaemia, endocarditis, meningitis, pneumonia, skin and wound sepsis (or) urinary tract infection'
   });
 
   Entries.insert({
-    lastName: 'Gonzalez',
-    firstName: 'Maria',
-    location: 'Pana',
-    description: 'Was surveyed with Martinez',
-    userId: nico._id,
-    author: nico.profile.name,
+    eventName: 'Bartonella elizabethae (Massachusetts, 1986)',
+    refEventName: 'Jones et al. SI',
+    disease: 'Endocarditis ',
+    refDisease: '149 ',
+    zoonoticType: 'zoonotic-wildlife',
+    refZoonoticType: '459',
+    userId: alexa._id,
+    author: alexa.profile.name,
     submitted: now - 10 * 3600 * 1000,
     commentsCount: 0
   });
 
   Entries.insert({
-    lastName: 'Romero',
-    firstName: 'Jose',
-    location: 'Rio',
-    description: 'Last person surveyed',
-    userId: nico._id,
-    author: nico.profile.name,
+    eventName: 'Chikungunya virus (West Kalimantan/Sumatra/Java Indonesia, 1982)',
+    refEventName: 'Jones et al. SI',
+    disease: 'Chikungunya fever',
+    refDisease: '238, 336, 360, 609',
+    zoonoticType: 'zoonotic-wildlife',
+    refZoonoticType: '360',
+    userId: alex._id,
+    author: alex.profile.name,
     submitted: now - 12 * 3600 * 1000,
     commentsCount: 0
   });
+
+  Entries.insert({
+    eventName: 'Ebola virus (Sudan, 1976)',
+    refEventName: 'Jones et al. SI',
+    disease: 'Haemorrhagic fever',
+    refDisease: '8',
+    zoonoticType: 'zoonotic-unkown',
+    refZoonoticType: '738',
+    userId: elizabeth._id,
+    author: elizabeth.profile.name,
+    submitted: now - 12 * 3600 * 1000,
+    commentsCount: 0
+  });
+
 }
