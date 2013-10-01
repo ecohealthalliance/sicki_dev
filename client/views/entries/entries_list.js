bdd20f295376a0abd3091fd2a34d3efc3f101e4d
nicopresto <nicopresto@gmail.com>
2013-10-01 14:15:13 -0400

    fixed events/entries typos

--- client/views/entries/entries_list.js
+++ client/views/entries/entries_list.js
@@ -1,5 +1,20 @@
 Template.entriesList.helpers({
   entries: function() {
-    return Entries.find({}, {sort: {eventName: 1}});
+    return Entries.find({}, {sort: {eventName: 1}, limit: entriesHandle.limit()})
+  },
+
+  entriesReady: function() {
+    return ! entriesHandle.loading();
+  },
+  allEntriesLoaded: function() {
+    return ! entriesHandle.loading() && 
+      Entries.find().count() < entriesHandle.loaded();
+  }
+});
+
+Template.entriesList.events({
+  'click .load-more': function(e) {
+    e.preventDefault();
+    entriesHandle.loadNextPage();
   }
 });
